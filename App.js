2ec0360db7f10bb623ed9396c8526a9b133587bb
Paloma Chaves <palomacha100@gmail.com>
2023-11-07 13:11:42 -0300

    Inicia projeto

---
+++ App.js
@@ -0,0 +1,14 @@
+
+export const App = () => {
+  return (
+    <div className="App">
+      
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
+    </div>
+  );
+}
+
+export default App;
